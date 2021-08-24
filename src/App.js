76ef9091c686372d1416e2a0b12b95528dd7c7fd
Fabricio Capiglioni <fabriciokpi@gmail.com>
2021-08-24 19:29:32 -0300

    modifiqué el parrafo de app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto React Js de CorderHouse Capiglioni
         </p>
         <a
           className="App-link"
