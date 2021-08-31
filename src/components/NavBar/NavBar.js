d5c31e7f1ade970088118d287d3e1b2d5e3a0a8f
Fabricio Capiglioni <fabriciokpi@gmail.com>
2021-08-31 16:08:48 -0300

    modifique los href del navbar con "javascript:void(0);"

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,25 +10,25 @@ const NavBar = () => {
     return (
         <nav className="nav fixed-top">
             <div classname="container col-sm-2">
-                <a classname="navbar-brand" href="#/">
+                <a classname="navbar-brand" href="javascript:void(0);">
                     <img src={logo} alt="" width="50" height="50"></img>
                 </a>
             </div>
             <div className="navLinks col-sm-8">
-                <a className="nav-link active" aria-current="page" href="#/">Home</a>
+                <a className="nav-link active" aria-current="page" href="javascript:void(0);">Home</a>
                 <ul className="nav-link"> 
                     <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a className="nav-link dropdown-toggle" href="javascript:void(0);" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Categories
                         </a>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item"  href="#/"> <FontAwesomeIcon icon={faStar}/> Top Sales</a></li>
-                            <li><a className="dropdown-item" href="#/"> <FontAwesomeIcon icon={faMobileAlt}/> Smart Phones</a></li>
-                            <li><a className="dropdown-item" href="#/"> <FontAwesomeIcon icon={faFireAlt}/> Hot Sales</a></li>
+                            <li><a className="dropdown-item"  href="javascript:void(0);"> <FontAwesomeIcon icon={faStar}/> Top Sales</a></li>
+                            <li><a className="dropdown-item" href="javascript:void(0);"> <FontAwesomeIcon icon={faMobileAlt}/> Smart Phones</a></li>
+                            <li><a className="dropdown-item" href="javascript:void(0);"> <FontAwesomeIcon icon={faFireAlt}/> Hot Sales</a></li>
                         </ul>
                     </li>
                 </ul>
-                <a className="nav-link" href="#/">About Us</a>
+                <a className="nav-link" href="javascript:void(0);">About Us</a>
             </div>
             <CartWidget/>
         </nav>
