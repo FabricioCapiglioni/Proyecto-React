8649fe6ffefb7c40dfbcef2dd0000fa698be21fb
Fabricio Capiglioni <fabriciokpi@gmail.com>
2021-08-31 15:57:39 -0300

    Agregué la sección main

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import NavBar from './components/NavBar/NavBar';
 import Carousel from './components/Carousel/Carousel.js';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <NavBar/>
         <Carousel/> 
-        
-        
       </header>
+      <body>
+        <main>
+        <ItemListContainer/>
+        </main>
+      </body>
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,12 @@
+import './ItemListContainer.css'
+
+const ItemListContainer = () => {
+    return ( 
+        <div>
+            <h1 className="title">¡Sorry!</h1>
+            <p className="pItem">This Site is Under Construction</p>
+        </div>
+    )
+}
+
+export default ItemListContainer;
