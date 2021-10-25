d56a369e2298c676d2c3486fb6eb49d0cffb95cd
Fabricio Capiglioni <fabriciokpi@gmail.com>
2021-10-24 23:38:52 -0300

    modifique el footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,28 +3,40 @@ import { NavLink } from 'react-router-dom';
 import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { useEffect, useState, useContext } from 'react'
+import { getCategories } from '../../assets/Services/firebase/firebase'
+import NotificationContext from '../../context/NotificationContext'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const Footer = () => {
+
+    const [categories, setCategories] = useState()
+    const { setNotification } = useContext(NotificationContext)
+
+    useEffect(() => {
+        getCategories().then(categories => {
+            setCategories(categories)
+        }).catch((error) => {
+            setNotification('error', error, 2000)
+        })
+        return () => {
+            setCategories()
+        }
+    }, [setNotification])
 
     return (
         <footer className="footer">
             <div className="navLinks col-sm-12">
                 <NavLink to='/' className="nav-link active" aria-current="page" href="dangerouslySetInnerHTML">Home</NavLink>
-                <ul className="nav-link"> 
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="dangerouslySetInnerHTML" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Brands
-                        </a>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">                            
-                            <NavLink to={`/category/apple`} key="01" className="dropdown-item" ><li>Apple</li></NavLink>
-                            <NavLink to={`/category/asus`} key="02" className="dropdown-item" ><li>Asus</li></NavLink>
-                            <NavLink to={`/category/google`} key="03" className="dropdown-item" ><li>Google</li></NavLink>
-                            <NavLink to={`/category/samsung`} key="04" className="dropdown-item" ><li>Samsung</li></NavLink>
-                            <NavLink to={`/category/xiaomi`} key="05" className="dropdown-item" ><li>Xiaomi</li></NavLink>                            
-                        </ul>
-                    </li>
-                </ul>
+                <NavDropdown title="Brands" className="nav-link" href="!#" id="collasible-nav-dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        
+                        {categories?.map(category =>
+                            <NavLink to={`/category/${category.name}`} key={category.id} className="dropdown-item" >
+                                <li>{category.description}</li>
+                            </NavLink>)
+                        }
+
+                    </ NavDropdown>
                 <NavLink to='/contact' className="nav-link" aria-current="page" href="dangerouslySetInnerHTML">Contact Us</NavLink>
             </div>
             <div className="copyrights">
